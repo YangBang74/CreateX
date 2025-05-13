ca2f9f8463324a7197d9389da8da7113ba125c8d
Yang Bang74 <crowbeat.47@gmail.com>
2025-05-13 16:24:20 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { RouterView } from 'vue-router'
+import Header from '@/components/Header.vue'
+</script>
+
+<template>
+  <Header />
+  <main class="pt-10">
+    <RouterView />
+  </main>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<script setup></script>
+
+<template>
+  <section class="hero">
+    <div class="container"><div class="div">Hi</div></div>
+  </section>
+</template>
+
+<style scoped>
+.hero {
+  background: url('/home/hero-bg.png') no-repeat center center / cover;
+}
+</style>
